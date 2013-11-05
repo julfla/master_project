f1468b75970e9f9d5bd4e9ce65fe7cb7604653b8
Julien FLAJOLLET <julien@flajollet.fr>
2013-11-05 11:31:55 +0900

    Refactor and test

--- test_integration.sh
+++ test_integration.sh
@@ -1,44 +1,45 @@
 #!/bin/sh
 
-echo "This test will download, convert, compute descriptors and distance map"
-
 #TODO
-#add the plot scripts and test them
-#add to git repo
-#add scripts to cmakes and redefine targets
-# test mesh randomness
+#Fix the downloader to get full description, add tags and similars models
+
+
 
 dataset=~/Documents/Recherche/dataset
 tool_dir=$(dirname $(readlink -f $0))/tools
 bin_dir=$(dirname $(readlink -f $0))/bin
+#used to create a tmp folder
 #tmp_dir=$(mktemp -d)
-tmp_dir=~/Bureau/tmp; mkdir -p $tmp_dir
+tmp_dir=/tmp/dataset;
 
+
+echo "This test will download, convert, compute descriptors and distance map"
+mkdir -p $tmp_dir
 cd $tmp_dir
-#rm */*.dist;
 
+#mkdir -p mug; cd mug
+#echo "Downloading skp for keywords \"mug\""
+#ruby $tool_dir/warehouse_scrapper.rb mug
+#cd ..
 
-mkdir -p mug; cd mug
-echo "Downloading skp for keywords \"mug\""
-ruby $tool_dir/warehouse_scrapper.rb mug
-cd ..
-
-#echo "Link to the dataset files"
-#for dir in $dataset/*/; do
-#	mkdir -p $(basename $dir);
-#	for file in $dir/*.skp; do ln -sf $file $(basename $dir)/$(basename $file); done;
-#done
+echo "Link to the dataset files"
+for dir in $dataset/*/; do
+	mkdir -p $(basename $dir);
+	for file in $dir/*.skp; do ln -sf $file $(basename $dir)/$(basename $file); done;
+done
+#rm -r fork spoon
 
 echo "Convert skp files to tri format"
-#for file in */*.skp; do WINEDEBUG=-all, wine $bin_dir/skp2tri.exe -i $file -o ${file%.skp}.tri; done;
+for file in */*.skp; do WINEDEBUG=-all, wine $bin_dir/skp2tri.exe -i $file -o ${file%.skp}.tri; done;
 
 echo "Computation of descriptors"
-#for file in */*.tri; do $bin_dir/distribution -i $file -o ${file%.tri}.dist; done;
+for file in */*.tri; do $bin_dir/distribution -i $file -o ${file%.tri}.dist; done;
+
 
 echo "Plot descriptors"
 for file in */*.dist; do
-	$bin_dir/distribution --csv --conv -i $file -o ${file%.dist}.png
-	R --slave -f $tool_dir/plot_raw_dist.R --args $file ${file%.dist}.png
+	$bin_dir/distribution --csv --conv -i $file -o ${file%.dist}.csv
+	R --slave -f $tool_dir/plot_raw_dist.R --args ${file%.dist}.csv ${file%.dist}.png
 done;
 
 echo "Computation of distance matrix"
@@ -48,13 +49,12 @@ for file in $files; do matrix=$matrix" "$(basename --suffix=".dist" -a $file); d
 matrix=$matrix"\n"
 for file1 in $files; do
 	matrix=$matrix$(basename --suffix=".dist" -a $file1)
-#	for file2 in $files; do matrix=$matrix" "$($bin_dir/distance $file1 $file2); done;
+	for file2 in $files; do matrix=$matrix" "$($bin_dir/distance $file1 $file2); done;
 	matrix=$matrix"\n"
 done
 echo $matrix > matrix_distance.m
 
 R --slave -f $tool_dir/heatmap.R --args "matrix_distance.m" 
 
-#rm -r $tmp_dir
 
 
