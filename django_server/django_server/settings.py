3619f0d10f0b5ffdf3b79eef1598e87d875267e4
Julien FLAJOLLET <julien@flajollet.fr>
2014-01-17 17:49:59 +0900

    Skeleton with of django using mongodb

--- django_server/django_server/settings.py
+++ django_server/django_server/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'testapp',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -56,10 +57,10 @@ WSGI_APPLICATION = 'django_server.wsgi.application'
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+   'default' : {
+      'ENGINE' : 'django_mongodb_engine',
+      'NAME' : 'my_database'
+   }
 }
 
 # Internationalization
