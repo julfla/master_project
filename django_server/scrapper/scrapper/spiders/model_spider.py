46a95c4c312f0155f6a268e9c19b3a40baffbd0d
Julien FLAJOLLET <julien@flajollet.fr>
2014-01-21 16:42:31 +0900

    Cast scrapper item field to utf8

--- django_server/scrapper/scrapper/spiders/model_spider.py
+++ django_server/scrapper/scrapper/spiders/model_spider.py
@@ -23,13 +23,21 @@ class ModelSpider(Spider):
         models = []
         models_div = sel.xpath('//div[@class="searchresult"]')
 
+        def extract_uft8(field):
+            return field.extract()[0].encode('utf8')
+
         for model_div in models_div:
             model = ModelItem()
-            model['google_id'] = model_div.xpath('@id').extract()
-            model['link_image'] = model_div.xpath('table/tr/td/a/img/@src').extract()
+            site_basename = "http://sketchup.google.com"
+            model['google_id'] = extract_uft8(model_div.xpath('@id'))
+            model['link_image'] = site_basename + extract_uft8(model_div.
+                xpath('table/tr/td/a/img/@src'))
             right_side_info = model_div.xpath('table/tr/td[@valign="top"]')
-            model['title'] = right_side_info.xpath('div/span[@id="bylinetitle"]/a/@title').extract()
-            model['link_full_description'] = right_side_info.xpath('div/span[@id="bylinetitle"]/a/@href').extract()
-            model['link_skp'] = right_side_info.xpath('span/div/span/a[@class="dwnld"]/@href').extract()
+            model['title'] = extract_uft8(right_side_info.
+                xpath('div/span[@id="bylinetitle"]/a/@title'))
+            #model['link_full_description'] = right_side_info.
+            #   xpath('div/span[@id="bylinetitle"]/a/@href').extract()
+            model['link_skp'] = site_basename + extract_uft8(right_side_info.
+                xpath('span/div/span/a[@class="dwnld"]/@href'))
             models.append(model)
         return models
