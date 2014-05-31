f215591c7093a32429a0ceaeb43906ee0bf5eff7
Julien FLAJOLLET <julien@flajollet.fr>
2014-05-31 12:34:31 +0900

    Add tests for partial_view

--- partial_pointcloud/test/partial_pointcloud_test.cpp
+++ partial_pointcloud/test/partial_pointcloud_test.cpp
@@ -7,15 +7,25 @@ class Partial_Pointcloud_Test : public CppUnit::TestFixture{
   
   CPPUNIT_TEST_SUITE( Partial_Pointcloud_Test );
   CPPUNIT_TEST( testNoSegfaultOnDestruction );
+  CPPUNIT_TEST( testEnoughPointInCloud );
   CPPUNIT_TEST_SUITE_END();
 
 public:
 
-  void testNoSegfaultOnDestruction()
-  {
+  void testNoSegfaultOnDestruction() {
     PartialViewComputer * comp = new PartialViewComputer();
     delete comp;
     CPPUNIT_ASSERT( true );
+  }
+
+  void testEnoughPointInCloud() {
+  	PartialViewComputer comp;
+  	std::string path = "../partial_pointcloud/test/fa61e604661d4aa66658ecd96794a1cd.tri";
+  	comp.loadMesh( path );
+  	DefaultPointCloud cloud = comp.compute_view( 0.0, 0.0);
+  	// One cloud can have width * height = 480000 points
+  	// We expect at least 30% of it
+  	CPPUNIT_ASSERT( cloud.size() >= 480000 * 0.33);
   }
 
 };
