2a47ab24fc1889898d65c5cb08e303fb08a37ae7
Julien FLAJOLLET <julien@flajollet.fr>
2014-06-08 15:03:04 +0900

    process keep-descriptors argument

--- django_server/identifier/models.py
+++ django_server/identifier/models.py
@@ -1,111 +1,104 @@
+
 from django.db import models
-from djangotoolbox.fields import DictField, SetField, EmbeddedModelField
+from djangotoolbox.fields import DictField, SetField
 
 from django.utils.six import with_metaclass
 
 from sketchup_models.models import SketchupModel
-from shape_distribution.models import ShapeDistribution, SHAPE_DISTRIBUTION_SIZE
+from shape_distribution.models import (ShapeDistribution,
+                                       SHAPE_DISTRIBUTION_SIZE)
 from partial_view.models import PartialView
 
-from sklearn import svm
+from sklearn import svm, multiclass
 import pickle
 import numpy
 
+
 class SVCField(with_metaclass(models.SubfieldBase, models.Field)):
 
+    """ A django field conresponding to sklearn classifier objects. """
+
     import inspect
-    svm_classifier_classes = inspect.getmembers(svm.classes, inspect.isclass)
-    svm_classifier_classes = map( lambda x: x[1], svm_classifier_classes)
-    # Recreate python object from db
+    svm_classifier_classes = inspect.getmembers(svm, inspect.isclass)
+    svm_classifier_classes += inspect.getmembers(multiclass, inspect.isclass)
+    svm_classifier_classes = map(lambda x: x[1], svm_classifier_classes)
+
     def to_python(self, value):
+        """ Recreate python object from db. """
         if value.__class__ in self.svm_classifier_classes:
             return value
         elif value is not None and len(value) > 0:
             return pickle.loads(value)
         return None
 
-    # Serialize python object to be stored in db
     def get_prep_value(self, value):
+        """ Serialize python object to be stored in db. """
         if value.__class__ in self.svm_classifier_classes:
-            return pickle.dumps( value )
+            return pickle.dumps(value)
         else:
             return value
 
+
 class Identifier(models.Model):
+
+    """ Identifier for pointcloud objects. """
 
     classifier = SVCField()
     dict_categories = DictField(SetField, default=dict())
 
-    @staticmethod
-    def instance():
-        try:
-            return Identifier.objects.all()[0]
-        except:
-            return Identifier()
-
     def identify_with_proba(self, pointcloud):
-        """
-        Returns the category of the pointcloud object if its category is known.
-        If not, throws an exception.
-        """
-        if len( self.dict_categories ) < 1 :
+        """ Return the category of the pointcloud object. """
+        if len(self.dict_categories) < 1:
             print "No category cannot identify"
             raise IndexError("Identifier is empty.")
 
         data = ShapeDistribution.compute(pointcloud).as_numpy_array
-        result_proba = self.classifier.decision_function( data )
-        result_idx = int(self.classifier.predict( data )[0])
+        result_proba = self.classifier.decision_function(data)
+        result_idx = int(self.classifier.predict(data)[0])
         result_name = self.dict_categories.keys()[result_idx]
         return (result_name, result_proba)
 
     def identify(self, pointcloud):
+        """ Return only the identification result, no probability. """
         (result_name, result_proba) = self.identify_with_proba(pointcloud)
         return result_name
 
     def add_models(self, models, category_name):
-        """
-        Add models to the category.
-        If the category is not known yet, it will be created.
-        """
-        if not category_name in self.dict_categories:
+        """ Add models to the category, create the category if necessary. """
+        if category_name not in self.dict_categories:
             self.dict_categories[category_name] = set()
             print "The category {} has been created.".format(category_name)
         category = self.dict_categories[category_name]
-        print "Adding models to category {}.".format( category_name )
+        print "Adding models to category {}.".format(category_name)
         for model in models:
-            category.add( model.google_id )
+            category.add(model.google_id)
 
     def train(self):
-        """
-        Train the classifier for all the models that it knows
-        """
-        if len( self.dict_categories ) < 2:
+        """ Train the classifier for all the models that it knows. """
+        if len(self.dict_categories) < 2:
             print "At least two categories are needed for training..."
             print "Training is skipped."
         (X, Y) = self._get_example_matrix()
         print "Training with {} categories and {} views.".format(
-            len(self.dict_categories), len(Y)
-            )
+            len(self.dict_categories), len(Y))
         self.classifier = svm.LinearSVC()
         print self.classifier.fit(X, Y)
 
-
     def _get_example_matrix(self):
-        """
-        Return the input matrix of example (X)
-        """
-        X = numpy.zeros( [0, SHAPE_DISTRIBUTION_SIZE] )
-        Y = numpy.zeros( [0, 1] )
-        for idx, (category, model_ids) in enumerate(self.dict_categories.items()):
-            arr = numpy.zeros( (0, SHAPE_DISTRIBUTION_SIZE ) )
+        """ Return the input matrix of example (X). """
+        X = numpy.zeros([0, SHAPE_DISTRIBUTION_SIZE])
+        Y = numpy.zeros([0, 1])
+        for idx, (category, model_ids) \
+         in enumerate(self.dict_categories.items()):
+            arr = numpy.zeros((0, SHAPE_DISTRIBUTION_SIZE))
             for model_id in model_ids:
                 model = SketchupModel.find_google_id(model_id)
                 if model.partialview_set.count() == 0:
                     PartialView.compute_all_views(model)
                 for view in model.partialview_set.all():
-                    arr = numpy.vstack( [arr, view.distribution.as_numpy_array] )
-            X = numpy.vstack( [X, arr] )
+                    arr = numpy.vstack([arr, view.distribution.as_numpy_array])
+            X = numpy.vstack([X, arr])
             n_views = arr.shape[0]
-            Y = numpy.vstack( [Y, idx * numpy.ones([ n_views, 1])])
+            Y = numpy.vstack([Y, idx * numpy.ones([n_views, 1])])
         Y = numpy.ravel(Y)
         return (X, Y)
--- django_server/system_evaluation/management/commands/full_evaluation.py
+++ django_server/system_evaluation/management/commands/full_evaluation.py
@@ -1,6 +1,9 @@
-from django.core.management.base import BaseCommand, CommandError
+""" Define a Django Command for system evaluation. """
+
+from django.core.management.base import BaseCommand
 from optparse import make_option
-import pickle, json
+import pickle
+import json
 
 from sketchup_models.models import SketchupModel
 from system_evaluation.models import Example
@@ -9,9 +12,18 @@ from pointcloud.models import PointCloud
 from sklearn.svm import LinearSVC, SVC
 from sklearn.multiclass import OneVsOneClassifier, OneVsRestClassifier
 
+
 class Command(BaseCommand):
-    help = ('Perform learning from a preset set of models,'
-        + ' and test vs all bowls and bananas')
+
+    """ Django Command for classification evaluation. """
+
+    dataset = None
+    identifier = None
+    pending_examples = []
+    done_examples = []
+
+    help = ('Perform learning from a set of models,'
+            ' and test it vs the dataset.')
 
     classifiers_available = {
         'LinearSVC': LinearSVC(),
@@ -22,136 +34,143 @@ class Command(BaseCommand):
 
     option_list = BaseCommand.option_list + (
         make_option('-d', '--dataset',
-            dest='dataset_file',
-            default='evaluation_dataset.json.sample',
-            help='Json file containing model ids of learnign dataset.'),
+                    dest='dataset_file',
+                    default='evaluation_dataset.json.sample',
+                    help='Json file with model ids of learnign dataset.'),
         make_option('-r', '--restrict',
-            dest='categories',
-            default=None,
-            help='Restrict the dataset to a list of category. Separate categories with comma.'),
+                    dest='categories',
+                    default=None,
+                    help=('Restrict the dataset to a list of category.'
+                          ' Separate categories with comma.')),
         make_option('-o', '--save',
-            dest='save_file',
-            default=None,
-            help=('When interupted, the task state is store in this file,' +
-                ' so it can be resumed. Default to load parameter if used.')),
+                    dest='save_file',
+                    default=None,
+                    help=('When interupted, the task state is store in this '
+                          'file, so it can be resumed. '
+                          'Default to load parameter if used.')),
         make_option('-i', '--load',
-            dest='load_file',
-            default=None,
-            help='Resume task from this file.'),
+                    dest='load_file',
+                    default=None,
+                    help='Resume task from this file.'),
         make_option('-a', '--analyze',
-            dest='analyze',
-            action='store_true',
-            default=False,
-            help='Display results of evaluation.'),
+                    dest='analyze',
+                    action='store_true',
+                    default=False,
+                    help='Display results of evaluation.'),
         make_option('-l', '--learning-only',
-            dest='learning',
-            action='store_true',
-            default=False,
-            help='Performs the learning only, do not evaluate.'),
+                    dest='learning',
+                    action='store_true',
+                    default=False,
+                    help='Performs the learning only, do not evaluate.'),
         make_option('-k', '--keep-descriptors',
-            dest='keep-descriptors',
-            action='store_true',
-            default=False,
-            help='Keep the descritors value in the output file.'),
+                    dest='keep-descriptors',
+                    action='store_true',
+                    default=False,
+                    help='Keep the descritors value in the output file.'),
         make_option('-c', '--classifier',
-            dest='classifier_type',
-            default='LinearSVC',
-            help='Kind of classifier to use.')
+                    dest='classifier_type',
+                    default='LinearSVC',
+                    help='Kind of classifier to use.')
         )
 
-    def create_parser(self, prog_name, subcommand):
-        parser = super(Command, self).create_parser(prog_name, subcommand)
-        parser.disable_interspersed_args()
-        return parser
-
     def handle(self, *args, **options):
+        """ Handle the command call. """
         if options['load_file']:
             if not options['save_file']:
                 options['save_file'] = options['load_file']
-            self.load( options['load_file'] )
-        else:
-            self.pending_examples = None
-            self.done_examples = None
-            with open( options['dataset_file'] ) as f:
+            self.load(options['load_file'])
+        if self.dataset is None:
+            with open(options['dataset_file']) as f:
                 self.dataset = json.load(f)
             if not options['categories'] is None:
                 categories = options['categories'].split(',')
                 for category in categories:
-                    if not category in self.dataset:
-                        print "ERROR : {} in not included in the dataset".format(category)
+                    if category not in self.dataset:
+                        print("ERROR : {}".format(category),
+                              " in not included in the dataset")
                         return
-                print self.dataset
                 self.dataset = {k: self.dataset[k] for k in categories}
-                print self.dataset
             self.perform_learning(options['classifier_type'])
-        if options['learning']:
+        if options['learning']:  # We stop after learning
             if options['save_file']:
-                print 'Saving state into {}.'.format( options['save_file'] )
-                self.dump( options['save_file'] )
+                print 'Saving state into {}.'.format(options['save_file'])
+                self.dump(options['save_file'])
             return
-        if self.pending_examples is None and self.done_examples is None:
+        if not self.pending_examples and not self.done_examples:
             self.initialize_list_examples()
         try:
             while len(self.pending_examples):
-                example = self.pending_examples[0]
-                self.process_example( example )
-                self.done_examples.append( example )
-                self.pending_examples.remove(example)
+                self.process_example(self.pending_examples[0], options)
+            if 'analyze' in options:
+                self.analyse_results()
+        # The process can be stopped and saved for restart
         except KeyboardInterrupt:
-            if options['save_file']:
-                print 'Saving state into {}.'.format( options['save_file'] )
-                self.dump( options['save_file'] )
-            return
-        if options['analyze']:
-            self.analyse_results()
-        if options['save_file']:
-            print 'Saving state into {}.'.format( options['save_file'] )
-            self.dump( options['save_file'] )
+            pass
+        if 'save_file' in options:
+            print 'Saving state into {}.'.format(options['save_file'])
+            self.dump(options['save_file'])
 
     def perform_learning(self, classifier_type):
+        """ Perfrom the learning from the dataset. """
         # Retreiving the dataset models
         models = {}
         for category in self.dataset.keys():
             models[category] = []
             for google_id in self.dataset[category]:
-                models[category].append( SketchupModel.find_google_id(google_id) )
+                models[category].append(
+                    SketchupModel.find_google_id(google_id))
         # Training
         print 'Training using a {} classifier.'.format(classifier_type)
         classifier = self.classifiers_available[classifier_type]
         self.identifier = Identifier(classifier=classifier)
         for category in models.keys():
-            self.identifier.add_models( models[category], category)
+            self.identifier.add_models(models[category], category)
         self.identifier.train()
 
     def initialize_list_examples(self):
+        """ Generate the pending examples list from the the database. """
         self.pending_examples = []
         self.done_examples = []
         categories = self.dataset.keys()
-        map_f = lambda example: {'name': example.name, 'object_name': example.object_name, 'expected': example.category}
-        for example in Example.filter_categories( categories ).iterator():
-            self.pending_examples.append( map_f(example) )
+        map_f = lambda example: {'name': example.name,
+                                 'object_name': example.object_name,
+                                 'expected': example.category}
+        for example in Example.filter_categories(categories).iterator():
+            self.pending_examples.append(map_f(example))
 
-    def process_example(self, example):
+    def process_example(self, example, options):
+        """ Process one example, and remove it from the pending list. """
         # Display of completion
         number_done = len(self.done_examples)
         number_total = number_done + len(self.pending_examples)
-        print 'Dealing with {} ( {}/{}, {}% )'.format( example['name'],
-            number_done + 1, number_total, 100 * number_done / number_total )
+        print 'Dealing with {} ( {}/{}, {}% )'.format(
+            example['name'],
+            number_done + 1,
+            number_total,
+            100 * number_done / number_total)
         pcd_file = Example.objects.get(name=example['name']).pcd_file
-        cloud = PointCloud.load_pcd( pcd_file.name )
+        cloud = PointCloud.load_pcd(pcd_file.name)
         (category, proba) = self.identifier.identify_with_proba(cloud)
         example['actual'] = category
         example['proba'] = proba
+        #  we may include the descriptors to the outputs
+        if 'keep-descriptors' in options:
+            from shape_distribution.models import ShapeDistribution
+            shape_distribution = ShapeDistribution.compute(cloud)
+            example['shape_distribution'] = shape_distribution.as_numpy_array
         # display of results
-        print '{} has been identified has a {}, {}'.format(
-            example['name'], example['actual'], example['proba'] )
+        print '    {} has been identified has a {}, {}'.format(
+            example['name'], example['actual'], example['proba'])
+        self.done_examples.append(example)
+        self.pending_examples.remove(example)
 
     def analyse_results(self):
-
+        """ Analyze the results and print it in the terminal. """
         def result_group_by(examples, key, displayed_name=None):
             from collections import defaultdict
-            if displayed_name is None: displayed_name = key
-            results = defaultdict(lambda : defaultdict(list))
+            if displayed_name is None:
+                displayed_name = key
+            results = defaultdict(lambda: defaultdict(list))
             for example in examples:
                 key_value = example[key]
                 results[key_value]['all'].append(example)
@@ -160,42 +179,49 @@ class Command(BaseCommand):
                 else:
                     results[key_value]['negatives'].append(example)
             for group, item in results.items():
-                results[group]['percentage'] = 100 * len(item['positives']) / len(item['all'])
+                results[group]['percentage'] = (100 * len(item['positives']) /
+                                                len(item['all']))
 
-            sorted_results = sorted(results.items(), key=lambda x: x[1]['percentage'] )
+            sorted_results = sorted(results.items(),
+                                    key=lambda x: x[1]['percentage'])
             print 'Results by {}'.format(displayed_name)
             for group, item in sorted_results:
                 error_stats = defaultdict(int)
                 for example in item['negatives']:
                     error_stats[example['actual']] += 1
-                sorted_error_starts = sorted(error_stats.iteritems(), key=lambda x: x[1], reverse=True)
-                detail_result = ', '.join( map( lambda x: "{}: {}".format(x[0], x[1]), sorted_error_starts ) )
+                sorted_error_starts = sorted(error_stats.iteritems(),
+                                             key=lambda x: x[1], reverse=True)
+                detail_result = ', '.join(
+                    map(lambda x: "{}: {}".format(x[0], x[1]),
+                        sorted_error_starts))
 
                 print '{} : {}% (total: {}) | Errors : {}'.format(
                     group, item['percentage'], len(item['all']), detail_result)
             print ''
 
         examples = self.done_examples
-        positives = [a for a in examples if a['expected'] ==  a['actual']]
-        print "Identification of {} examples.".format( len(examples) )
-        print "Overall results : {}%".format(100 * len(positives)/len(examples))
+        positives = [a for a in examples if a['expected'] == a['actual']]
+        print "Identification of {} examples.".format(len(examples))
+        print "Overall results : {}%".format(
+            100 * len(positives)/len(examples))
         print ''
         result_group_by(examples, 'expected', 'category')
         result_group_by(examples, 'object_name', 'object')
 
     def dump(self, state_file_path):
+        """ Dump the process in a homemade format. """
         state = {}
         state['dataset'] = self.dataset
         state['done_examples'] = self.done_examples
         state['pending_examples'] = self.pending_examples
         state['identifier'] = self.identifier
         with open(state_file_path, 'wb') as handle:
-            pickle.dump( state, handle )
-        print "State has been saved to {}.".format( state_file_path )
+            pickle.dump(state, handle)
+        print "State has been saved to {}.".format(state_file_path)
 
     def load(self, state_file_path):
+        """ Load the process state to be restarted. """
         with open(state_file_path, 'r') as handle:
-            state = pickle.load( handle )
-        self.__dict__.update( state )
-        print "State has been restored from {}.".format( state_file_path )
-
+            state = pickle.load(handle)
+        self.__dict__.update(state)
+        print "State has been restored from {}.".format(state_file_path)
