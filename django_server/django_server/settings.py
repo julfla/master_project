d9c289f7f7a451238f85baa3e1f07bbe12a91074
Julien FLAJOLLET <julien@flajollet.fr>
2014-07-14 13:37:58 +0900

    FIX for static files

--- django_server/django_server/settings.py
+++ django_server/django_server/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = (
     'singleton_models',
     'bootstrap3',
     'common',
+    'django_server',
     'identifier',
     'pointcloud',
     'sketchup_models',
@@ -116,5 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+    '/var/www/static/',
+)
 
 MEDIA_ROOT = 'media/'
