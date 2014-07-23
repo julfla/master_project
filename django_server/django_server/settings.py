a5d3592123c58b8432e6dceb27ac7bdbc4b94737
Julien FLAJOLLET <julien@flajollet.fr>
2014-07-23 15:10:01 +0900

    Change DB for user study so that is we can continue on develop at the same time

--- django_server/django_server/settings.py
+++ django_server/django_server/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'django_server.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django_mongodb_engine',
-        'NAME': 'my_database'
+        'NAME': 'user_study'
     }
 }
 
